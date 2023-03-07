72a34c4efa3e37e586d9e72ae0f1d911e998d7da
Zhi Li <lzblack@users.noreply.github.com>
2023-03-07 12:28:21 -0500

    Update json_example.py

--- code/json_example.py
+++ code/json_example.py
@@ -7,9 +7,11 @@ country_code = 'us'
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
 
 # print(url)
-f = urllib.request.urlopen(url)
-response_text = f.read().decode('utf-8')
-response_data = json.loads(response_text)
+
+with urllib.request.urlopen(url) as f:
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    
 print(response_data)
 
 # How do we get current temperature?
