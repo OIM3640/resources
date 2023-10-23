d1bd7779ec02f424cb8b1eae043d5a2c458d2f10
Zhi Li <lzblack@users.noreply.github.com>
2023-10-23 11:35:45 -0400

    modified:   code/people_in_space.py

--- code/people_in_space.py
+++ code/people_in_space.py
@@ -1,12 +1,17 @@
 import urllib.request
 import json
+import pprint
 
 url = "http://api.open-notify.org/astros.json"
 
 with urllib.request.urlopen(url) as f:
     response_text = f.read().decode('utf-8')
-    j = json.loads(response_text) # j is a dictionary
-    print(j) 
+    # print(response_text)
+    # print(type(response_text))
+    data = json.loads(response_text)
+    # print(data)
+    # print(type(data))
+    pprint.pprint(data)
 
 # Can you print number of people in the space?
 
