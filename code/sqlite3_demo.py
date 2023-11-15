18e5a57d47f4a8209fa2ca2dfe8f7973fbbb3c5e
Zhi Li <lzblack@users.noreply.github.com>
2023-11-15 13:50:22 -0500

    Update sqlite3_demo.py

--- code/sqlite3_demo.py
+++ code/sqlite3_demo.py
@@ -1,27 +1,61 @@
 import sqlite3
 
-db = sqlite3.connect('data/stocks.db')
-c = db.cursor()
-c.execute('create table portfolio (symbol text, shares integer, price real)')
-db.commit()
 
-# stocks = [
-#     ('GOOG', 100, 1093.05),
-#     ('AAPL', 50, 217.68),
-#     ('FB', 150, 155.42),
-#     ('MSFT', 100, 109.03),
-#     ('SNAP', 75, 6.88)
-# ]
-# c.executemany('insert into portfolio values (?,?,?)', stocks)
-# db.commit()
-
-# for row in db.execute('select * from portfolio'):
-#     print(row)
+def create_portfolio_table():
+    with sqlite3.connect("data/stocks.db") as db:
+        cursor = db.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS portfolio (
+                symbol TEXT,
+                shares INTEGER,
+                price REAL
+            )
+            """
+        )
 
 
-# min_price = 150
+def insert_stocks(stocks):
+    with sqlite3.connect("data/stocks.db") as db:
+        cursor = db.cursor()
+        cursor.executemany("INSERT INTO portfolio VALUES (?,?,?)", stocks)
 
-# print('\nExpensive Stocks:')
-# for row in db.execute('select * from portfolio where price > ?',
-#                       (min_price,)):
-#     print(row)
+
+def display_portfolio():
+    with sqlite3.connect("data/stocks.db") as db:
+        cursor = db.cursor()
+        for row in cursor.execute("SELECT * FROM portfolio"):
+            print(row)
+
+
+def display_expensive_stocks(price):
+    with sqlite3.connect("data/stocks.db") as db:
+        cursor = db.cursor()
+        results = cursor.execute("SELECT * FROM portfolio WHERE price > ?", (price,))
+        for row in results:
+            print(row)
+
+
+def main():
+    """"""
+    create_portfolio_table()
+
+    # stocks = [
+    #     ("AAPL", 50, 189.03),
+    #     ("FB", 150, 332.58),
+    #     ("GOOG", 100, 136.05),
+    #     ("MSFT", 100, 368.97),
+    #     ("NVDA", 100, 487.00),
+    #     ("TSLA", 100, 242.79),
+    # ]
+    # insert_stocks(stocks)
+
+    # display_portfolio()
+
+    # print("\nExpensive Stocks:")
+    # min_price = 300
+    # display_expensive_stocks(min_price)
+
+
+if __name__ == "__main__":
+    main()
