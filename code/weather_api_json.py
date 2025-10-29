050fc8a216bc9d88786bfa385b905d4285dad7b8
Zhi Li <lzblack@users.noreply.github.com>
2025-10-29 14:24:19 -0400

    Rename files

---
+++ code/weather_api_json.py
@@ -0,0 +1,4 @@
+weather_data = {'coord': {'lon': -71.2926, 'lat': 42.2965}, 'weather': [{'id': 804, 'main': 'Clouds', 'description': 'overcast clouds', 'icon': '04d'}], 'base': 'stations', 'main': {'temp': 282.48, 'feels_like': 280.53, 'temp_min': 281.21, 'temp_max': 283.4, 'pressure': 1021, 'humidity': 82, 'sea_level': 1021, 'grnd_level': 1014}, 'visibility': 10000, 'wind': {'speed': 3.6, 'deg': 20}, 'clouds': {'all': 100}, 'dt': 1761762137, 'sys': {'type': 2, 'id': 2093979, 'country': 'US', 'sunrise': 1761736488, 'sunset': 1761774165}, 'timezone': -14400, 'id': 4954738, 'name': 'Wellesley', 'cod': 200}
+
+
+# What is current temperature?
